nickalobiondar/nimble

commit 02945e57b5abf0ff303d5d722f1fba4006717d21

prepare Nimble for the upcoming compiler API (#489)
* prepare Nimble for the upcoming compiler API changes

* attempt to make tests green

* make some tests green with the upcoming compiler API

src/nimblepkg/version.nim
@@ -166,7 +166,7 @@ proc parseVersionRange*(s: string): VersionRange =
   var i = 0
   var op = ""
   var version = ""
-  while true:
+  while i < s.len:
     case s[i]
     of '>', '<', '=':
       op.add(s[i])
@@ -189,10 +189,6 @@ proc parseVersionRange*(s: string): VersionRange =
     of '0'..'9', '.':
       version.add(s[i])
 
-    of '\0':
-      result = makeRange(version, op)
-      break
-
     of ' ':
       # Make sure '0.9 8.03' is not allowed.
       if version != "" and i < s.len:
@@ -204,6 +200,7 @@ proc parseVersionRange*(s: string): VersionRange =
       raise newException(ParseVersionError,
           "Unexpected char in version range '" & s & "': " & s[i])
     inc(i)
+  result = makeRange(version, op)
 
 proc toVersionRange*(ver: Version): VersionRange =
   ## Converts a version to either a verEq or verSpecial VersionRange.

src/nimblepkg/nimscriptsupport.nim
@@ -269,7 +269,7 @@ proc getLibVersion(lib: string): Version =
     return system.NimVersion.newVersion()
 
 when declared(ModuleGraph):
-  var graph: ModuleGraph
+  var graph = newModuleGraph()
 
 proc execScript(scriptName: string, flags: Flags, options: Options): PSym =
   ## Executes the specified script. Returns the script's module symbol.

commit 125baf719911b5ab3225f28d5dbd44489b8fdf5f

Fixes regression introduced by 02945e57b5ab.

src/nimblepkg/version.nim
@@ -182,7 +182,7 @@ proc parseVersionRange*(s: string): VersionRange =
         raise newException(ParseVersionError,
             "Having more than one `&` in a version range is pointless")
 
-      break
+      return
 
     of '0'..'9', '.':
       version.add(s[i])
@@ -291,16 +291,16 @@ when isMainModule:
   doAssert(newVersion("1.0") < newVersion("1.4"))
   doAssert(newVersion("1.0.1") > newVersion("1.0"))
   doAssert(newVersion("1.0.6") <= newVersion("1.0.6"))
-  #doAssert(not withinRange(newVersion("0.1.0"), parseVersionRange("> 0.1")))
+  doAssert(not withinRange(newVersion("0.1.0"), parseVersionRange("> 0.1")))
   doAssert(not (newVersion("0.1.0") < newVersion("0.1")))
   doAssert(not (newVersion("0.1.0") > newVersion("0.1")))
   doAssert(newVersion("0.1.0") < newVersion("0.1.0.0.1"))
   doAssert(newVersion("0.1.0") <= newVersion("0.1"))
 
   var inter1 = parseVersionRange(">= 1.0 & <= 1.5")
+  doAssert inter1.kind == verIntersect
   var inter2 = parseVersionRange("1.0")
   doAssert(inter2.kind == verEq)
-  #echo(parseVersionRange(">= 0.8 0.9"))
 
   doAssert(not withinRange(newVersion("1.5.1"), inter1))
   doAssert(withinRange(newVersion("1.0.2.3.4.5.6.7.8.9.10.11.12"), inter1))
@@ -350,5 +350,8 @@ when isMainModule:
   # toVersionRange tests
   doAssert toVersionRange(newVersion("#head")).kind == verSpecial
   doAssert toVersionRange(newVersion("0.2.0")).kind == verEq
+
+  # Something raised on IRC
+  doAssert newVersion("1") == newVersion("1.0")
 
   echo("Everything works!")